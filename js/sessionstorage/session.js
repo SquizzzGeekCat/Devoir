4a3ac22b0b3224eadc59ca8e744c82397783f68b
Ludivine CHOMIK <chomik.lu@gmail.com>
2024-10-24 09:19:57 +0200

    suite exo

---
+++ js/sessionstorage/session.js
@@ -0,0 +1,4 @@
+const nom = document.getElementById("name").value;
+const voie = document.getElementById("numVoie").value;
+const rue = document.getElementById("rue");
+const cp = document.getElementById("cp");
